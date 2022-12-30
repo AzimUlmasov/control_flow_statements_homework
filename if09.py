3b51d5646f81e5aaa117da1cb49aea34ef3cf581
Azim Ulmasov <azimulmasov2@gmail.com>
2022-12-30 23:25:57 +0500

    09

--- if09.py
+++ if09.py
@@ -9,4 +9,7 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    ans = a % 10 * 10 + a // 10
+    return ans <= a
+
+print(main(35))
